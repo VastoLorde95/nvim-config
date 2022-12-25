da1ceb34ebb5301dcedc54043c3b081100653d2c
Aditya Paliwal <adityapaliwal95@gmail.com>
2022-12-24 18:34:16 -0600

    Change colorscheme

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,6 +1,1 @@
--- Sets up the colorscheme from plugins
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true
-vim.opt.background = "dark"
-
 vim.cmd[[colorscheme tokyonight]]
