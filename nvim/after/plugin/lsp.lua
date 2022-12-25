7f7c1fb467570f1eba4ae76ea9f7cbb14fa9c5f6
Aditya Paliwal <adityapaliwal95@gmail.com>
2022-12-24 22:10:47 -0600

    Some formatting

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -11,7 +11,6 @@ local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
     local opts = { noremap = true, silent = true }
     vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
@@ -26,18 +25,17 @@ local on_attach = function(client, bufnr)
     -- whether it is global or local. So we can remap gD from jumping to "local"
     -- declaration, to instead go to a defintion.
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
     -- I don't know what these do yet
-    -- vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
     -- vim.keymap.set('n', '<Leader>k', vim.lsp.buf.signature_help, bufopts)
     -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     -- vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
