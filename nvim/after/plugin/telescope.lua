ad33e4191a56fe8f19921290b32ec606275ae1b2
Aditya Paliwal <adityapaliwal95@gmail.com>
2022-12-25 01:18:29 -0600

    Apply noremap to all telescope remaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,34 +4,35 @@
 -- Run :Telescope builtin to list all builtins and their documentation
 
 local builtin = require('telescope.builtin')
+local options = { noremap = true }
 
 -- Vim pickers
 
 -- find files
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, options)
 
 -- find grep. Will respect .gitignore
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, options)
 
 -- find buffer 
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, options)
 
 -- find help
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, options)
 
 -- TODO(apaliwal): Implement fuzzy search for any pattern within the current buffer 
 -- and jump to that location on <cr>
 
 -- LSP related keymappings
 -- find references
-vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, options)
 
 -- find definitions
-vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, options)
 
 -- find errors
-vim.keymap.set('n', '<leader>fe', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>fe', builtin.diagnostics, options)
 
 -- Treesitter related keymappings
 -- find treesitter - search for function names and variables through treesitter
-vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, options)
