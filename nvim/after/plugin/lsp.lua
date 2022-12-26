e67df2ed7fde107c6bc7a4698579d7e8ceb08545
Aditya Paliwal <adityapaliwal95@gmail.com>
2022-12-26 01:37:18 -0600

    Remaps for diagnostics are independent of the existence of an LSP - move them out of on_attach

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,17 +5,17 @@
 
 -- See `:help vim.diagnostic.*` for documentation on any of the functions below
 
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    local opts = { noremap = true, silent = true }
-    vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
