ccff6742084cac3c12f680fc4415c633efb925be
Aditya Paliwal <adityapaliwal95@gmail.com>
2022-12-26 17:43:48 -0600

    Create a module for autocmds, create one to enable nu and rnu in vim help docs.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,8 @@
+-- Use vim.fn to call vim-functions or user-funcions
+-- Use vim.api for nvim api functions
+
 require("settings")
 require("maps")
 require("plugins")
 require("mytabline")
+require("autocommands")
---
+++ nvim/lua/autocommands.lua
@@ -0,0 +1,3 @@
+-- Enable number and relativenumber in vim help  docs.
+vim.cmd[[autocmd FileType help setlocal number]]
+vim.cmd[[autocmd FileType help setlocal relativenumber]]
