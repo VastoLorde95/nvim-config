16f889a0bbcae7b3b935435800c84489de6aba71
Aditya Paliwal <adityapaliwal95@gmail.com>
2023-02-18 14:50:23 -0600

    Delete unused plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,9 +31,6 @@ return require('packer').startup(function(use)
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-nvim-lua'
-    --use 'hrsh7th/cmp-path'
-    --use 'hrsh7th/cmp-cmdline'
-    --use 'ray-x/cmp-treesitter'
 
     -- Treesitter --
     use {
