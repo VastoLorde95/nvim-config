9a7bc7302eb7d68c91978115f5c490eaf3a46f8a
Aditya Paliwal <adityapaliwal95@gmail.com>
2023-09-16 00:24:13 -0400

    Simplify treesitter setup

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,41 +2,18 @@
 -- See :h nvim-treesitter-modules for a list of available modules
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "cpp", "lua", "python", },
+    ensure_installed = { "c", "cpp", "lua", "python", "bash"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = false,
 
-    -- List of parsers to ignore installing (for "all")
-    -- ignore_install = { "javascript" },
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-    -- This wasn't working right with Python so disable for now.
     indent = {
         enable = true,
-        disable = {'python'}
     },
 
-    highlight = { enable = true },
-    -- Select scopes based on which node you are on as per the tree sitter, expand this
-    -- scope by going up to the parent or going down into the child
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = '<Leader><Tab>',
-            scope_incremental = '<CR>',
-            node_incremental = '<Tab>',
-            node_decremental = '<S-Tab>',
-        },
-    },
+    highlight = { enable = true , additional_vim_regex_highlighting = false },
 }
-
--- TODO(apaliwal): Need to experiment with this a bit more before I get it working.
--- vim.wo.foldmethod = "expr"
--- vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
