997607bc758363224cd38650cdca0931cf229d31
Aditya Paliwal <adityapaliwal95@gmail.com>
2024-02-02 12:05:17 -0800

    Update treesitter.lua
    Add rust to treesitter ensure_installed.

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 -- See :h nvim-treesitter-modules for a list of available modules
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "cpp", "lua", "python", "bash"},
+    ensure_installed = { "c", "cpp", "lua", "python", "bash", "rust"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = true,
