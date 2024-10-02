f803b737bceb5290f64aa4d08d019feddfa4b61c
Aditya Paliwal <aditaypaliwal95@gmail.com>
2024-10-02 12:31:51 -0700

    Get rid of tsserver

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -85,23 +85,6 @@ require('lspconfig').bufls.setup{
     capabilities = capabilities,
 }
 
-require'lspconfig'.tsserver.setup{
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-        languages = {"javascript", "typescript", "vue"},
-      },
-    },
-  },
-  filetypes = {
-    "javascript",
-    "typescript",
-    "vue",
-  },
-}
-
 --require('lspconfig').helm_ls.setup {
     --on_attach = on_attach,
     --capabilities = capabilities,
